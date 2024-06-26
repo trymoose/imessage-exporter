6be960404b7f5ae04c08c824982aea0446104907
Christopher Sardegna <github@reagentx.net>
2024-06-25 21:09:38 -0700

    Better error messages

--- imessage-database/src/error/table.rs
+++ imessage-database/src/error/table.rs
@@ -21,11 +21,11 @@ pub enum TableError {
 impl Display for TableError {
     fn fmt(&self, fmt: &mut Formatter<'_>) -> Result {
         match self {
-            TableError::Attachment(why) => write!(fmt, "Failed to parse row: {why}"),
-            TableError::ChatToHandle(why) => write!(fmt, "Failed to parse row: {why}"),
-            TableError::Chat(why) => write!(fmt, "Failed to parse row: {why}"),
-            TableError::Handle(why) => write!(fmt, "Failed to parse row: {why}"),
-            TableError::Messages(why) => write!(fmt, "Failed to parse row: {why}"),
+            TableError::Attachment(why) => write!(fmt, "Failed to parse attachment row: {why}"),
+            TableError::ChatToHandle(why) => write!(fmt, "Failed to parse chat handle row: {why}"),
+            TableError::Chat(why) => write!(fmt, "Failed to parse chat row: {why}"),
+            TableError::Handle(why) => write!(fmt, "Failed to parse handle row: {why}"),
+            TableError::Messages(why) => write!(fmt, "Failed to parse messages row: {why}"),
             TableError::CannotConnect(why) => write!(fmt, "{why}"),
             TableError::CannotRead(why) => write!(fmt, "{why}"),
         }
