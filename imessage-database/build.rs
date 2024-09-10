3690fdd37a4202d575e75a7158f03e97a9bcfd8c
Christopher Sardegna <github@reagentx.net>
2024-09-09 21:35:36 -0700

    Compile handwriting protobuf in build step

---
+++ imessage-database/build.rs
@@ -0,0 +1,16 @@
+use std::{env, fs::copy, path::PathBuf};
+
+fn main() {
+    protobuf_codegen::Codegen::new()
+        .pure()
+        .input("src/message_types/handwriting/handwriting.proto")
+        .include(".")
+        .cargo_out_dir("p")
+        .run_from_script();
+
+    // Move generated file to correct location
+    let mut generated = PathBuf::from(env::var("OUT_DIR").unwrap());
+    generated.push("p");
+    generated.push("handwriting.rs");
+    copy(generated, "src/message_types/handwriting/handwriting_proto.rs").unwrap();
+}
