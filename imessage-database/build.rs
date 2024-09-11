9bffb7403cf124a907c1bc746316ad7c5a629228
Christopher Sardegna <github@reagentx.net>
2024-09-10 22:49:53 -0700

    Only run codegen if protobuf file is missing

--- imessage-database/build.rs
+++ imessage-database/build.rs
@@ -1,16 +1,26 @@
-use std::{env, fs::copy, path::PathBuf};
+use std::{
+    env,
+    fs::{copy, exists},
+    path::PathBuf,
+};
 
 fn main() {
-    protobuf_codegen::Codegen::new()
-        .pure()
-        .input("src/message_types/handwriting/handwriting.proto")
-        .include(".")
-        .cargo_out_dir("p")
-        .run_from_script();
+    if !exists("src/message_types/handwriting/handwriting_proto.rs").unwrap() {
+        protobuf_codegen::Codegen::new()
+            .pure()
+            .input("src/message_types/handwriting/handwriting.proto")
+            .include(".")
+            .cargo_out_dir("p")
+            .run_from_script();
 
-    // Move generated file to correct location
-    let mut generated = PathBuf::from(env::var("OUT_DIR").unwrap());
-    generated.push("p");
-    generated.push("handwriting.rs");
-    copy(generated, "src/message_types/handwriting/handwriting_proto.rs").unwrap();
+        // Move generated file to correct location
+        let mut generated = PathBuf::from(env::var("OUT_DIR").unwrap());
+        generated.push("p");
+        generated.push("handwriting.rs");
+        copy(
+            generated,
+            "src/message_types/handwriting/handwriting_proto.rs",
+        )
+        .unwrap();
+    }
 }
